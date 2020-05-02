a8ced97cd37e9695395d532ff06c4e7a1ed6d67d
git-moss <juergen.mossgraber@googlemail.com>
2020-05-02 17:24:54 +0200

    Added Part 15.

--- Part-15/HardwareAPITest2.control.js
+++ Part-15/HardwareAPITest2.control.js
@@ -1,7 +1,7 @@
 loadAPI(10);
 
 host.setShouldFailOnDeprecatedUse(true);
-host.defineController("Moss", "Hardware API Test 1", "1.0", "68e7b117-0999-496a-b5a0-16f533fc5da0", "Moss");
+host.defineController("Moss", "Hardware API Test 2", "1.0", "98ff9719-450a-4a5c-86fb-48ff171127ae", "Moss");
 host.defineMidiPorts(1, 0);
 
 function init()
@@ -23,6 +23,9 @@ function init()
    var absKnob = hardwareSurface.createAbsoluteHardwareKnob ("ABSOLUTE_KNOB");
    // RelativeHardwareKnob
    var relKnob = hardwareSurface.createRelativeHardwareKnob ("RELATIVE_KNOB");
+
+   // PianoKeyboard
+   var keyboard = hardwareSurface.createPianoKeyboard ("THE_KEYBOARD",49, 0, 36);
    
 
    // 2. Bind the controls to MIDI commands
@@ -32,6 +35,9 @@ function init()
    slider.setAdjustValueMatcher (port.createAbsoluteCCValueMatcher (channel, sliderControl));
    absKnob.setAdjustValueMatcher (port.createAbsoluteCCValueMatcher (channel, absKnobControl));
    relKnob.setAdjustValueMatcher (port.createRelativeBinOffsetCCValueMatcher (channel, relKnobControl, 127));
+
+   var noteInput = port.createNoteInput ("Hardware Test 2");
+   keyboard.setMidiIn (port);
 
 
    // 3. Bind the controls to actions / targets
@@ -43,6 +49,17 @@ function init()
    slider.setBinding (trackBank.getItemAt (0).volume ());
    absKnob.setBinding (trackBank.getItemAt (1).volume ());
    relKnob.setBinding (trackBank.getItemAt (2).volume ());
+
+
+   // 4. Layout the simulator UI
+
+   hardwareSurface.setPhysicalSize (200, 200);
+
+   playButton.setBounds(137.0, 133.5, 8.75, 7.0);
+   hardwareSurface.hardwareElementWithId("SLIDER_1").setBounds(106.0, 120.0, 10.0, 34.25);
+   hardwareSurface.hardwareElementWithId("ABSOLUTE_KNOB").setBounds(127.25, 12.25, 10.0, 10.0);
+   hardwareSurface.hardwareElementWithId("RELATIVE_KNOB").setBounds(127.25, 78.75, 10.0, 10.0);
+   hardwareSurface.hardwareElementWithId("THE_KEYBOARD").setBounds(4.5, 163.5, 191.0, 28.25);
 
    println("Initialized!");
 }
